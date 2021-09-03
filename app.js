d0ed7a6aed5f4de8625c7c55229dac6ed003abcd
Christopher Keith <chris.keith@gmail.com>
2021-09-02 17:01:57 -0700

    Run on https port, not http. Still not coming up in GCP.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'www')))
 
-const port = 80
+const port = 443
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'www/index.html'))
